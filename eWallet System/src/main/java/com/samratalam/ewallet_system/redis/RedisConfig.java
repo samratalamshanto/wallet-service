80e66532749cfdebe8e73a306a5c901f26dd84ed
Samrat Alam <samratalamshanto710@gmail.com>
2025-07-19 16:32:31 +0600

    Update RedisConfig.java

--- eWallet System/src/main/java/com/samratalam/ewallet_system/redis/RedisConfig.java
+++ eWallet System/src/main/java/com/samratalam/ewallet_system/redis/RedisConfig.java
@@ -8,7 +8,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-public class RedisConfig {
+public class RedisTemplateConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
